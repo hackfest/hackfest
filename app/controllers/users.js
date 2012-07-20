a174967d4737e35996e7d6fb9d8ee8714c0b01db
Madhusudhan Srinivasa <madhums8@gmail.com>
2012-07-20 21:42:00 +0530

    users controller

---
+++ app/controllers/users.js
@@ -0,0 +1,24 @@
+
+module.exports = function (app, passport) {
+
+  app.get('/login', function (req, res) {
+    res.render('users/login')
+  })
+
+  app.get('/auth/github', passport.authenticate('github'), function(req, res, next){
+    // The request will be redirected to GitHub for authentication, so this
+    // function will not be called.
+  })
+
+  app.get('/auth/github/callback', function(req, res, next) {
+    console.log(req.user)
+    // Successful authentication, redirect home.
+    res.redirect('/')
+  })
+
+  app.get('/logout', function(req, res){
+    req.logOut()
+    res.redirect('/')
+  })
+
+}
