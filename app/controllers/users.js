61a3a0a0299f82cb5608789ccff549a5c2bd14e4
Madhusudhan Srinivasa <madhums8@gmail.com>
2012-07-21 20:32:45 +0530

    add profile and hackers routes

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -3,6 +3,17 @@ var User = mongoose.model('User')
 
 module.exports = function (app, passport, auth) {
 
+  app.param('username', function (req, res, next, username) {
+    User
+      .findOne({ username: username })
+      .exec(function (err, profile) {
+        if (err) return next(err)
+        if (!profile) return next(new Error ('User not found'))
+        req.profile = profile
+        next()
+      })
+  })
+
   app.get('/login', function (req, res) {
     res.render('users/login')
   })
@@ -26,10 +37,28 @@ module.exports = function (app, passport, auth) {
     User
       .findOne({ _id: req.user._id })
       .exec(function (err, user) {
-        if (err) return res.render('400')
+        if (err) return res.render('404')
         if (!user) return res.render('404')
         res.render('users/account', {
             user: user
+        })
+      })
+  })
+
+  app.get('/profile/:username', ensureAuthenticated, function (req, res) {
+    res.render('users/profile', {
+      user: req.profile
+    })
+  })
+
+  app.get('/hackers', ensureAuthenticated, function (req, res) {
+    User
+      .find()
+      .exec(function (err, users) {
+        if (err) return res.render('404')
+        if (!users) return res.render('404')
+        res.render('users/hackers', {
+            users: users
         })
       })
   })
