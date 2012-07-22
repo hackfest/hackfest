a307d3e9c5a6d56d2485c69bd6496d3118dfd0ab
Madhusudhan Srinivasa <madhums8@gmail.com>
2012-07-23 00:00:42 +0530

    home page actions

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,9 +1,36 @@
 
+var Idea = mongoose.model('Idea')
+  , async = require('async')
+  , trending
+  , featured
+  , recent
+
+Idea.trending(function (err, ideas) { trending = ideas })
+Idea.recent(function (err, ideas) { recent = ideas })
+Idea.featured(function (err, ideas) { featured = ideas })
+
 module.exports = function (app, passport) {
 
   app.get('/', function (req, res) {
-    if (req.isAuthenticated())
-      res.render('home/dash')
+    if (req.isAuthenticated()) {
+
+      callback = function (err, ideas) { return ideas }
+
+      async.parallel({
+              trending: function(callback){
+                Idea.trending(callback)
+              }
+            , featured: function(callback){
+                Idea.featured(callback)
+              }
+            , recent: function(callback){
+                Idea.recent(callback)
+              }
+          }
+        , function(err, results) {
+            res.render('home/dash', results)
+      })
+    }
     else
       res.render('home/landing')
   })
