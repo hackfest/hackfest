03406c44690110e255fb33ade3ad2c9bae49bd5a
Madhusudhan Srinivasa <madhums8@gmail.com>
2012-07-23 00:00:23 +0530

    idea authorization middlewares

--- authorization.js
+++ authorization.js
@@ -2,10 +2,18 @@
 
 module.exports = {
 
-  requiresLogin: function (req, res, next) {
-    if (req.isAuthenticated()) { return next() }
-    res.redirect('/login')
-    next()
-  }
+    requiresLogin: function (req, res, next) {
+      if (req.isAuthenticated()) { return next() }
+      res.redirect('/login')
+      next()
+    }
+
+  , idea: {
+      hasAccess: function (req, res, next) {
+        if (req.idea.author._id.toString() !== req.user._id.toString())
+          res.redirect('/ideas/'+req.idea.id)
+        next()
+      }
+    }
 
 }
